237ec54a213593ec52314a97e6b8280d823eb2a5
Denis Markovic <markovic.denis@yahoo.de>
2025-12-07 03:02:48 +0100

    refactor: add parsePKHash method to ModelCriteria for improved primary key handling

--- src/Propel/Runtime/ActiveQuery/ModelCriteria.php
+++ src/Propel/Runtime/ActiveQuery/ModelCriteria.php
@@ -2689,4 +2689,19 @@ class ModelCriteria extends BaseModelCriteria
 
         return parent::addSelectColumn($name);
     }
+
+    /**
+     * Custom function to parse primary key hash
+     */
+    public function parsePKHash(mixed $key): ?string
+    {
+        if (is_string($key)) {
+            return $key;
+        }
+        if ($key === null || is_scalar($key) || is_callable([$key, '__toString'])) {
+            return (string) $key;
+        }
+
+        return null;
+    }
 }
